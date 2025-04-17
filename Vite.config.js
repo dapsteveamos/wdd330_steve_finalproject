024cc0fbf922d2c12f2badff1f85aafc64f34f4b
Steve Amos Daprim <dapsteveamos@gmail.com>
2025-04-17 09:11:53 +0000

    Update

--- Vite.config.js
+++ Vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'project', // no dot-slash needed
+  root: './project', // no dot-slash needed
   build: {
     outDir: '../dist', // this places the build output one level up
     emptyOutDir: true
